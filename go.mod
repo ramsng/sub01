4328a7b7401c98b6858be30dc968cb34211bff27
Ramasubramaniyam Azhagusundaram <rmsubramaniyam@gmail.com>
2024-01-03 22:06:10 -0800

    sec

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ramsng/sub01
+
+go 1.21.5
--- sub01.go
+++ sub01.go
@@ -3,10 +3,13 @@ package sub01
 import (
 	"fmt"
 	"math"
+
+	"github.com/ramsng/sub02"
 )
 
 func Depref(a float64, b float64) int {
-	fmt.Printf("** We are in package sub01 - inputs : %v ; %v", int(a), int(b))
+	fmt.Printf(sub02.Desc)
+	fmt.Printf("** We are in package sub01 - inputs : %d ; %d\n", int(a), int(b))
 	c := int((math.Pow(a, b)) * (math.Pow(b, a)))
 	return c
 }
