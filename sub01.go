727f10768a384b923cddf890339c4e1b6cdd607c
Ramasubramaniyam Azhagusundaram <rmsubramaniyam@gmail.com>
2024-01-03 21:13:29 -0800

    first commit

--- sub01.go
+++ sub01.go
@@ -6,7 +6,7 @@ import (
 )
 
 func Depref(a float64, b float64) int {
-	fmt.Printf("** We are in package sub01 - inputs : %d %d", a, b)
+	fmt.Printf("** We are in package sub01 - inputs : %v ; %v", int(a), int(b))
 	c := int((math.Pow(a, b)) * (math.Pow(b, a)))
 	return c
 }
