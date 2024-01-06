5d5c6e2082aff1f607368feda11e393554ebab10
Ramasubramaniyam Azhagusundaram <rmsubramaniyam@gmail.com>
2024-01-06 09:31:04 -0800

    fourth

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/ramsng/sub01
 
 go 1.21.5
+
+require github.com/ramsng/sub03 v0.0.0-20240106171327-68b1ef18c7bd
+
+require github.com/ramsng/sub02 v0.0.0-20240104063154-d721adcc95ee // indirect
--- sub01.go
+++ sub01.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"math"
 
-	"github.com/ramsng/sub02"
+	"github.com/ramsng/sub03"
 )
 
 func Depref(a float64, b float64) int {
-	fmt.Printf(sub02.Desc)
+	fmt.Printf(sub03.Praise())
 	fmt.Printf("** We are in package sub01 - inputs : %d ; %d\n", int(a), int(b))
 	c := int((math.Pow(a, b)) * (math.Pow(b, a)))
 	return c
